62d61a64f12579c055d110d69a498849da47cc0d
R10F <farrellriofa@gmail.com>
2022-11-08 10:40:17 +0700

    semantic tag pada task 2

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,7 +1,7 @@
 // Task No 1
 fetch("tmdbMovies.json")
   .then((response) => response.json())
-  .then((movies) =>
+  .then((movies) => {
     movies.map((list_view) => {
       let container = document.querySelector(".index");
       let div_img = document.createElement("div");
@@ -24,91 +24,82 @@ fetch("tmdbMovies.json")
       container.appendChild(div_img);
       container.appendChild(div_content);
       container.appendChild(div_icon);
-    })
-  );
+    });
+  });
 
 // TASK NO 2
 fetch("tmdbMovies.json")
   .then((response) => response.json())
   .then((movies) => {
     let movie = movies.slice(4, 5);
-    let container = document.querySelector(".movie");
+    let header = document.querySelector("header");
+    let main = document.querySelector("main");
     movie.map((tom_jerry) => {
-      container.innerHTML = `
-      <div class = "head"> 
-        <div class = "icon_head">
-          <img src = "panah_kiri.png" />
-          <span> Back </span>
-        </div>
-        
-        <div class = "text_head">
-          <h3> ${tom_jerry.title} </h3>
-        </div>
+      console.log(tom_jerry);
+      header.innerHTML = `
       
+      <div class = "icon_head">
+        <img src = "panah_kiri.png" />
+        <span> Back </span>
       </div>
 
-      <div class = "main">
-        <div class = "thumbnail">
-          <img src = "http://image.tmdb.org/t/p/w600_and_h900_bestv2/${tom_jerry.poster_path}"/>
-          <h3> ${tom_jerry.title} (${tom_jerry.release_date.slice(0, 4)})
+      <div class = "text_head">
+        <h3> ${tom_jerry.title} </h3>
+      </div>`;
+
+      main.innerHTML = `
+      <section class = "thumbnail">
+        <img src = "http://image.tmdb.org/t/p/w600_and_h900_bestv2/${tom_jerry.poster_path}"/>
+        <h3> ${tom_jerry.title} (${tom_jerry.release_date.slice(0, 4)})
+        <br>
+        <span>  ${tom_jerry.certification} | ${tom_jerry.release_date} | ${tom_jerry.original_language} </span>
+        </h3>
+      </section>
+
+      <section class = "overview"> <h3> Overview </h3> </section>
+
+      <section class = "isi_txt">
+        <div class = "director">
+          <p> ${tom_jerry.overview} </p>
+          <p> ${tom_jerry.directors[0].name}
           <br>
-          <span>  ${tom_jerry.certification} | ${tom_jerry.release_date} | ${tom_jerry.original_language} </span>     
-          </h3>
+          <span> Director </span>
+          </p>
         </div>
-        
-        <span> <h3> Overview </h3> </span>
-                        
-        <div class = "isi_txt">
-          <div class = "director">
-            <p> ${tom_jerry.overview} </p>
-            <p> ${tom_jerry.directors[0].name}
+
+        <div class = "writer">
+            <p> ${tom_jerry.writers[0].name}
             <br>
-            <span> Director </span>
+            <span> Writers </span>
             </p>
 
-          </div>
-           
-            
-            <div class = "writer"> 
-              <div class = "col" 
-                <p> ${tom_jerry.writers[0].name} 
-                <br>
-                <span> Writers </span>
-                </p>
-      
-                <p> ${tom_jerry.writers[2].name} 
-                <br>
-                <span> Writers </span>
-                </p>
-              </div>
-      
-              <div class = "col"
-                <p> ${tom_jerry.writers[1].name} 
-                <br>
-                <span> Writers </span>
-                </p>
-              </div>
-            </div> 
-      
-          <div class = "cast">
-              <div class = "col">
-                <p> ${tom_jerry.cast[0].name} 
-                <br>
-                <span> Casts </span>
-      
-                <p> ${tom_jerry.cast[2].name} 
-                <br>
-                <span> Casts </span>
-              </div>
-              
-              <div class = "col">
-                <p> ${tom_jerry.cast[1].name} 
-                <br>
-                <span> Casts </span>
-              </div>
-          </div>
+            <p> ${tom_jerry.writers[1].name}
+            <br>
+            <span> Writers </span>
+            </p>
+
+          
+            <p> ${tom_jerry.writers[2].name}
+            <br>
+            <span> Writers </span>
+            </p>
+        
         </div>
-      </div>     
-      `;
+
+        <div class = "cast">
+            
+            <p> ${tom_jerry.cast[0].name}
+            <br>
+            <span> Casts </span>
+
+            <p> ${tom_jerry.cast[1].name}
+            <br>
+            <span> Casts </span>
+                    
+            <p> ${tom_jerry.cast[2].name}
+            <br>
+            <span> Casts </span>
+        </div>
+    </div>`;
     });
   });
